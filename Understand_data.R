434b8b182d4ecf6c6c2d09432e589339658097fe
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 12:13:58 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -6,7 +6,7 @@ head(raw_data)
 unique(is.na(raw_data$Arrival.Delay.in.Minutes))
 
 
-cor(x=raw_data$Arrival.Delay.in.Minutes, y=raw_data$Origin.City)
+cor(x=raw_data$Arrival.Delay.in.Minutes, y=raw_data$Origin.City,method = c("pearson"))
 flight_time_in_minutes
 departure_delay_in_minutes
 
