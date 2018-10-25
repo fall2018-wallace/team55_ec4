a4023954acfa34e70ef46421c3a31cf8eb655c38
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 12:02:33 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -3,7 +3,7 @@ raw_data<-data
 str(raw_data)
 head(raw_data)
 
-unique(is.na(raw_data$Eating.and.Drinking.at.Airport))
+unique(is.na(raw_data$Shopping.Amount.at.Airport))
 
 
 arrival_delay_in_minutes
