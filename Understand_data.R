fcecb3cd5b8bffa5b99ea0d16e9998e8225c798c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 12:16:17 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -5,10 +5,6 @@ head(raw_data)
 
 unique(is.na(raw_data$Arrival.Delay.in.Minutes))
 
-
-flight_time_in_minutes
-departure_delay_in_minutes
-
 lm(formula = Arrival.Delay.in.Minutes ~ Origin.City, data = raw_data)
 
 
