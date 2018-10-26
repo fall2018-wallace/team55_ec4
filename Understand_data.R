1a3ccfed42e72454fa6fafe2ea7e3551b803da39
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-26 15:13:41 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -6,7 +6,7 @@ head(raw_data)
 unique(is.na(raw_data$Arrival.Delay.in.Minutes))
 
 raw_data
-
+head(raw_data$DayOfWeek)
 length(raw_data$Flight.cancelled == 'No')
 library('dplyr')
 abc <- raw_data %>% filter(is.na(Arrival.Delay.in.Minutes), Flight.cancelled == 'No')
