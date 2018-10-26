4b58482e3a4ef03746c851af79e4beba3397d5d6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-26 14:52:20 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -10,6 +10,7 @@ raw_data
 head(subset(raw_data, is.na(raw_data$Arrival.Delay.in.Minutes)),10)
 library('dplyr')
 abc <- raw_data %>% filter(is.na(Arrival.Delay.in.Minutes), Flight.cancelled == 'Yes')
+abc
 lm(formula = Arrival.Delay.in.Minutes ~ Orgin.City, data = raw_data)
 
 
