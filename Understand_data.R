6d2ed1fcc823076bf34d00c9d4426c31a6b599fb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-26 15:03:09 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -7,8 +7,7 @@ unique(is.na(raw_data$Arrival.Delay.in.Minutes))
 
 raw_data
 
-(count(subset(raw_data, raw_data$Flight.cancelled == 'Yes'))
-library('dplyr')
+(count(subset(raw_data, raw_data$Flight.cancelled == 'Yes'library('dplyr')
 abc <- raw_data %>% filter(is.na(Arrival.Delay.in.Minutes), Flight.cancelled == 'No')
 head(abc,10)
 max((raw_data$Arrival.Delay.in.Minutes), na.rm=TRUE)
