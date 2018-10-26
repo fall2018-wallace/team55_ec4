53b117cb748dbbebf0b3758b378fd7bafc4be4da
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-26 14:54:11 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -7,9 +7,9 @@ unique(is.na(raw_data$Arrival.Delay.in.Minutes))
 
 raw_data
 
-head(subset(raw_data, raw_data$Flight.cancelled == 'Yes'),10)
+head(subset(raw_data, raw_data$Flight.cancelled == 'No'),10)
 library('dplyr')
-abc <- raw_data %>% filter(is.na(Arrival.Delay.in.Minutes), Flight.cancelled == 'Yes')
+abc <- raw_data %>% filter(Arrival.Delay.in.Minutes == NA, Flight.cancelled == 'No')
 abc
 lm(formula = Arrival.Delay.in.Minutes ~ Orgin.City, data = raw_data)
 
