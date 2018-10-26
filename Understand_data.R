693561ab90bee2bb514217c22511b3221076efdd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-26 15:02:57 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -7,7 +7,7 @@ unique(is.na(raw_data$Arrival.Delay.in.Minutes))
 
 raw_data
 
-(length(subset(raw_data, raw_data$Flight.cancelled == 'Yes'))
+(count(subset(raw_data, raw_data$Flight.cancelled == 'Yes'))
 library('dplyr')
 abc <- raw_data %>% filter(is.na(Arrival.Delay.in.Minutes), Flight.cancelled == 'No')
 head(abc,10)
