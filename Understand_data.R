e45cf3c99c46d063125ed479024ad764428b3641
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-26 14:41:43 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -7,7 +7,7 @@ unique(is.na(raw_data$Arrival.Delay.in.Minutes))
 
 raw_data
 
-subset(raw_data, is.na(rawdata$Arrival.Delay.in.Minutes))
+subset(raw_data, is.na(raw_data$Arrival.Delay.in.Minutes))
 lm(formula = Arrival.Delay.in.Minutes ~ Orgin.City, data = raw_data)
 
 
