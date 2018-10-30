f656029ac719dd04d9f787876de5771b5e72cdc5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-30 16:15:54 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -8,7 +8,7 @@ unique(is.na(raw_data$Arrival.Delay.in.Minutes))
 raw_data
 unique(raw_data$Airline.Code)
 length(raw_data$Flight.cancelled == 'No')
-head(abc,10)
+
 max((raw_data$Arrival.Delay.in.Minutes), na.rm=TRUE)
 lm(formula = Arrival.Delay.in.Minutes ~ Orgin.City, data = raw_data)
 
