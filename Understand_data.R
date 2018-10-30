5b5805cefea4f8bda321aba44704d5817ac11312
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-30 19:59:46 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -3,15 +3,6 @@ raw_data<-data
 str(raw_data)
 head(raw_data)
 
-unique(is.na(raw_data$Arrival.Delay.in.Minutes))
-
-raw_data
-unique(raw_data$Airline.Code)
-length(raw_data$Flight.cancelled == 'No')
-
-max((raw_data$Arrival.Delay.in.Minutes), na.rm=TRUE)
-lm(formula = Satisfaction ~ Shopping.Amount.at.Airport, data = raw_data)
-
 attach(data)
 abc <- plot(Satisfaction, Shopping.Amount.at.Airport)
 
