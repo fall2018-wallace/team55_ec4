57fc5739cab2c707dc54717769d9274624b11723
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-30 16:15:32 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -8,8 +8,6 @@ unique(is.na(raw_data$Arrival.Delay.in.Minutes))
 raw_data
 unique(raw_data$Airline.Code)
 length(raw_data$Flight.cancelled == 'No')
-library('dplyr')
-abc <- raw_data %>% filter(is.na(Arrival.Delay.in.Minutes), Flight.cancelled == 'No')
 head(abc,10)
 max((raw_data$Arrival.Delay.in.Minutes), na.rm=TRUE)
 lm(formula = Arrival.Delay.in.Minutes ~ Orgin.City, data = raw_data)
