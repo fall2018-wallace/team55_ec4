911dd253bc15401912793f19e727cb202ef96144
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 14:28:49 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -3,9 +3,7 @@ raw_data<-data
 str(raw_data)
 head(raw_data)
 
-#any(is.na(raw_data))
-#unique(colnames(raw_data[is.na(raw_data),]))
-
 # This data has 28 variables  and 129889 observations
 # Satisfaction is the dependent variable and all the other columns are independent variables
 # The client is Southeast hence this project will have comparisons between Southeast and other providers
+# the date column will be converted into month and day of week to determine if time of
