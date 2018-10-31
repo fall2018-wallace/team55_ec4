0ab6b900d9d6fb34aef76c448628d3c1543e4498
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 14:11:15 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -7,3 +7,4 @@ head(raw_data)
 #unique(colnames(raw_data[is.na(raw_data),]))
 
 # This data has 28 variables  and 129889 observations
+# Satisfaction is the dependent variable and all the other columns are independent variables
