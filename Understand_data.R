5c70ec5af78477a1a26bee869960846bbb3bf7e9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 14:30:08 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -5,6 +5,7 @@ head(raw_data)
 
 # This data has 28 variables  and 129889 observations
 # Satisfaction is the dependent variable and all the other columns are independent variables
-# The client is Southeast hence this project will have comparisons between Southeast and other providers
-# the date column will be converted into month and day of week to determine if time of
+# The client is Southeast hence this project will have comparisons between Southeast 
+# and other providers
+# The date column will be converted into month and day of week to determine if time of
 # year and time of week have any significant effect on the Satisfaction
