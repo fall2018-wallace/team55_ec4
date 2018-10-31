b2a80caee69eba979bf6ad6221006145ffd0433a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 15:45:40 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -5,5 +5,7 @@ clean_data<-raw_data
 sapply(raw_data,function(y)unique(y))
 
 # the Satisfaction variable has 3 abnormal values which need to be replaced or removed
+
+#conversion to numeric converts the abnprmal values to NA
 clean_data$Satisfaction <- as.numeric(as.character(t(dataframe$Satisfaction)))
 clean_data$Satisfaction[is.na(dataframe$Satisfaction)] <- 3.5
