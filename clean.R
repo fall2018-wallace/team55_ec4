0f2f257079bbbd7dc29cf2fcd689bb49c1d0bd44
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 15:44:23 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,8 @@
 
 clean_data<-raw_data
+
+# find unique values in each column to check for abnormal values
+sapply(raw_data,function(y)unique(y))
+
+# the Satisfaction variable has 3 abnormal values which need to be replaced or removed
+
