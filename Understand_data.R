3c4ecca81982d5ebe9c463f708946129f13796eb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-30 20:13:05 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -2,10 +2,5 @@
 raw_data<-data
 
 
-attach(raw_data)
-min<-min(Shopping.Amount.at.Airport)
-max<-max(Shopping.Amount.at.Airport)
-clim = c(min,max)
-abc <- plot(Satisfaction, Shopping.Amount.at.Airport, xlim=clim)
-
-
+dataframe$Satisfaction <- as.numeric(dataframe$Satisfaction)
+cor(dataframe$Shopping.Amount.at.Airport, dataframe$Satisfaction)
