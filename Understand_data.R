81bd544a17bb788445e594dbb07986fcb8a6e93d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 14:39:23 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -9,4 +9,4 @@ head(raw_data)
 # and other providers
 # The date column will be converted into month and day of week to determine if time of
 # year and time of week have any significant effect on the Satisfaction
-lm(Satisfaction~Gender ,data=raw_data)
+#lm(Satisfaction~Gender ,data=raw_data)
