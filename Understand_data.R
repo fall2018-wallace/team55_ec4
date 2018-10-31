87bf6718ba822860e709c963af43610238b1cd7c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-30 20:05:34 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -3,6 +3,7 @@ raw_data<-data
 
 
 attach(raw_data)
+min<-min(Shopping.Amount.at.Airport)
 clim = c(min(Shopping.Amount.at.Airport),max(Shopping.Amount.at.Airport))
 abc <- plot(Satisfaction, Shopping.Amount.at.Airport, xlim=clim)
 
