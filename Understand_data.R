a87c188ddd1688d56cdb3faf595c35436c393b7b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 23:12:23 -0400

    Saving Understand_data.R

--- Understand_data.R
+++ Understand_data.R
@@ -1,7 +1,7 @@
 
 raw_data<-data
 str(raw_data)
-count(raw_data$Arrival.Delay.in.Minutes)
+length(raw_data$Arrival.Delay.in.Minutes)
 
 raw_data$Satisfaction <- as.numeric(raw_data$Satisfaction)
 cor(raw_data$Shopping.Amount.at.Airport, raw_data$Satisfaction)
