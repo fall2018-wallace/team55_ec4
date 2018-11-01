1783fdf40c962d026e64fbf50923061c589b3414
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 12:13:25 -0400

    Saving Exploratory_Data_Analysis5.R

--- Exploratory_Data_Analysis5.R
+++ Exploratory_Data_Analysis5.R
@@ -1,1 +1,7 @@
 
+library(ggplot2)
+library(dplyr)
+df<-clean_data
+#using this for Age
+satisfactionDelay<-ggplot(df,aes(Departure.Delay.in.Minutes,Satisfaction))+geom_count()+facet_grid(southeast ~ .)+stat_summary(aes(y=Satisfaction),fun.y = "mean", colour = "red", size = 2, geom = "point")
+cor(df$Departure.Delay.in.Minutes,df$Satisfaction)
