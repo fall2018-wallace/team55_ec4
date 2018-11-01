93fda08a5888ef199fc06acfa6110f8aca1f9eeb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 11:10:37 -0400

    force updated content when merge failed

--- Understand_data.R
+++ Understand_data.R
@@ -1,4 +1,16 @@
 
 raw_data<-data
-str(data)
-head(data)
+#str(raw_data)
+#head(raw_data)
+
+# This data has 28 variables  and 129889 observations
+# Satisfaction is the dependent variable and all the other columns are independent variables
+# The client is Southeast hence this project will have comparisons between Southeast 
+# and other providers
+# The date column will be converted into month and day of week to determine if time of
+# year and time of week have any significant effect on the Satisfaction
+
+# find unique values in each column to check for abnormal values
+sapply(raw_data,function(y)unique(y))
+
+
