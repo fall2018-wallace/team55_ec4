7e82fa7c57cb4c4003d2c62eee2a5d3cb5ce6cc4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 12:21:35 -0400

    Saving Exploratory_Data_Analysis5.R

--- Exploratory_Data_Analysis5.R
+++ Exploratory_Data_Analysis5.R
@@ -14,6 +14,8 @@ cor(df$Departure.Delay.in.Minutes,df$Satisfaction)
 satisfactionArrivalDelay<-ggplot(df,aes(Arrival.Delay.in.Minutes,Satisfaction))+geom_count()+facet_grid(southeast ~ .)+stat_summary(aes(y=Satisfaction),fun.y = "mean", colour = "red", size = 2, geom = "point")
 cor(df$Arrival.Delay.in.Minutes,df$Satisfaction)
 
+
+#In this plot, customer satisfactions can be seen by flights which were cancelled as well.
 satisfactionCancelled<-ggplot(df,aes(Flight.cancelled,Satisfaction))+geom_count()+facet_grid(southeast ~ .)+stat_summary(aes(y=Satisfaction),fun.y = "mean", colour = "red", size = 2, geom = "point")
 
 satisfactionFlightTime<-ggplot(df,aes(Flight.time.in.minutes,Satisfaction))+geom_count()+facet_grid(southeast ~ .)+stat_summary(aes(y=Satisfaction),fun.y = "mean", colour = "red", size = 2, geom = "point")
