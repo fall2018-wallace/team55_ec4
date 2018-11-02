7c11c381f5112fcf05cf9a5de52d8a5c7718a409
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 21:45:09 -0400

    Saving Exploratoty_Data_Analysis4.R

--- Exploratoty_Data_Analysis4.R
+++ Exploratoty_Data_Analysis4.R
@@ -60,3 +60,4 @@ fD4=fD4+geom_count()
 fD4=fD4+ ggtitle("Departure delay")
 fD4
 #the rest of Satisfaction seems similar for all Airlines
+#all the departure delays are below 500 Minutes for southeast Airlines
