7a8a8f0c613a2f0099e1cfec314a8cd451c576ee
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 21:05:26 -0400

    Saving Exploratoty_Data_Analysis4.R

--- Exploratoty_Data_Analysis4.R
+++ Exploratoty_Data_Analysis4.R
@@ -5,6 +5,7 @@ library(dplyr)
 sat=clean_data
 
 #convert the target variable to numeric
+#to perfrom various EDA operations
 sat$Satisfaction=as.numeric((as.character((t(sat$Satisfaction)))))
 class(sat$Flight.date)
 sat$Flight.date=as.Date(sat$Flight.date, format="%m/%d/%Y")
