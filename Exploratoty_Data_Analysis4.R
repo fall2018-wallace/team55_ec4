e4f4b23d4ad9ff97ca04965de822b19ba8d20528
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 20:56:37 -0400

    Saving Exploratoty_Data_Analysis4.R

--- Exploratoty_Data_Analysis4.R
+++ Exploratoty_Data_Analysis4.R
@@ -2,3 +2,7 @@
 library(ggplot2)
 library(dplyr)
 sat=clean_data
+sat$Satisfaction=as.numeric((as.character((t(sat$Satisfaction)))))
+class(sat$Flight.date)
+sat$Flight.date=as.Date(sat$Flight.date, format="%m/%d/%Y")
+class(sat$Flight.date)
