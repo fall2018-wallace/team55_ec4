2739b34f076a361a333e083ed9d3ac5d3e60e062
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-15 11:05:22 -0500

    Saving model_generation3.R

--- model_generation3.R
+++ model_generation3.R
@@ -1,2 +1,12 @@
 
-model3<-'model3'
+happyCust <- 0
+med <- median(clean_data$Satisfaction)
+happyCust[clean_data$Satisfaction>=med] <- 1
+happyCust[clean_data$Satisfaction<med] <- 0
+
+happyCust <- as.factor(happyCust)
+clean_data1 <- cbind(clean_data,happyCust)
+#clean_data1 <- clean_data1[,-c(1)]
+
+model2 <- lm(happyCust~Airline.Status+Age+Gender+Price.Sensitivity+No.of.Flights.p.a.+ X..of.Flight.with.other.Airlines+Type.of.Travel+Shopping.Amount.at.Airport+Class+Day.of.Month+Airline.Code+Orgin.City+Destination.City+Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes+Arrival.Delay.greater.5.Mins+Eating.and.Drinking.at.Airport,data=clean_data)
+summary(model2)
