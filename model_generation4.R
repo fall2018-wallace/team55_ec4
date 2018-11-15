216803c9fac10a41f4d4d5e23f4076df2228bb4a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 21:59:17 -0500

    Saving model_generation4.R

--- model_generation4.R
+++ model_generation4.R
@@ -1,2 +1,9 @@
 
-model4<-'model4'
+lm(Satisfaction ~ Airline.Status + Age + Gender +
+           Price.Sensitivity + Year.of.First.Flight + No.of.Flights.p.a. +
+              Type.of.Travel +
+              Shopping.Amount.at.Airport +
+              Class +
+             Scheduled.Departure.Hour +
+             Departure.Delay.in.Minutes +
+             Arrival.Delay.greater.5.Mins + week_days, data = clean_data )
