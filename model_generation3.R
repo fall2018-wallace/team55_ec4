fe0a45928ce5e9b911ccdf31442de9a577c64d7a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-29 00:07:41 -0500

    Saving model_generation3.R

--- model_generation3.R
+++ model_generation3.R
@@ -11,7 +11,7 @@ clean_data1 <- clean_data1[,-c(1)]
 #model3 <- glm(happyCust~Airline.Status+Age+Gender+Price.Sensitivity+No.of.Flights.p.a.+ X..of.Flight.with.other.Airlines+Type.of.Travel+Shopping.Amount.at.Airport+Class+Day.of.Month+Airline.Code+Orgin.City+Destination.City+Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes+Arrival.Delay.greater.5.Mins+Eating.and.Drinking.at.Airport,data=clean_data1)
 #summary(model3)
 
-sapply(clean_data1,function(x) sum(is.na(x)))
+
 clean_data1$Departure.Delay.in.Minutes[is.na(clean_data1$Departure.Delay.in.Minutes)] <- mean(clean_data1$Departure.Delay.in.Minutes,na.rm=T)
 clean_data1$Arrival.Delay.in.Minutes[is.na(clean_data1$Arrival.Delay.in.Minutes)] <- mean(clean_data1$Arrival.Delay.in.Minutes,na.rm=T)
 clean_data1$Flight.time.in.minutes[is.na(clean_data1$Departure.Delay.in.Minutes)] <- mean(clean_data1$Flight.time.in.minutes,na.rm=T)
