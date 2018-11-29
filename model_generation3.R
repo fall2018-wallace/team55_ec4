0a058a160c4695b17a4ddb341b2910255c960cb4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-29 00:27:46 -0500

    Saving model_generation3.R

--- model_generation3.R
+++ model_generation3.R
@@ -1,8 +1,8 @@
 
 happyCust <- 0
 med <- median(clean_data$Satisfaction)
-happyCust[clean_data$Satisfaction>=med] <- "YES"
-happyCust[clean_data$Satisfaction<med] <- "NO"
+happyCust[clean_data$Satisfaction>=med] <- 2
+happyCust[clean_data$Satisfaction<med] <- 1
 
 happyCust <- as.factor(happyCust)
 clean_data1 <- cbind(clean_data,happyCust)
