47889c4e942d3d89914fcdacbe501194b88d2558
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-01 12:49:13 -0500

    Saving evaluation.R

--- evaluation.R
+++ evaluation.R
@@ -1,7 +1,19 @@
 
 logtraindata <- clean_data1
 logtestdata <- test
+happyCust <- 1
+med <- median(logtestdata$Satisfaction)
+happyCust[logtestdata$Satisfaction>=med] <- 3
+happyCust[logtestdata$Satisfaction<med] <- 2
 
+happyCust <- as.factor(happyCust)
+logtestdata <- cbind(logtestdata,happyCust)
+logtestdata <- logtestdata[,-c(1)]
+
+
+logtestdata$Departure.Delay.in.Minutes[is.na(logtestdata$Departure.Delay.in.Minutes)] <- mean(logtestdata$Departure.Delay.in.Minutes,na.rm=T)
+logtestdata$Arrival.Delay.in.Minutes[is.na(logtestdata$Arrival.Delay.in.Minutes)] <- mean(logtestdata$Arrival.Delay.in.Minutes,na.rm=T)
+logtestdata$Flight.time.in.minutes[is.na(logtestdata$Flight.time.in.minutes)] <- mean(logtestdata$Flight.time.in.minutes,na.rm=T)
 logpred <- predict(model3,logtestdata,type="response")
 #round(logpred)
 str(clean_data)
