63a7e07965c3c50be852fd85462640f2e464064e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-01 12:43:22 -0500

    Saving evaluation.R

--- evaluation.R
+++ evaluation.R
@@ -2,6 +2,6 @@
 logtraindata <- clean_data1
 logtestdata <- test
 
-logpred <- predict(model3,testdata,type="response")
+logpred <- predict(model3,logtestdata,type="response")
 #round(logpred)
 str(clean_data)
