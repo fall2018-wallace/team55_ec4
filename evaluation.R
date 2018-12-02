225306bf6b7ea611b7bc2f45eaebde38aa2184ce
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-01 21:08:20 -0500

    Saving evaluation.R

--- evaluation.R
+++ evaluation.R
@@ -18,6 +18,7 @@ pos_or_neg <- ifelse(logpred > 0.5, 3, 2)
 happyCusttest <- factor(pos_or_neg)
 x <- table(happyCust,happyCusttest)
 error <- (x[1,1]+x[2,2])/sum(x)
+error
 #match1 <- match(happyCusttest,happyCust)
 #matchnegwords2 <- happyCust[which(match!=0)]
 #match1
