b31d61a9d1ff707f5b4e538634541fbf770cac7e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 21:10:24 -0500

    Saving model_generation1.R

--- model_generation1.R
+++ model_generation1.R
@@ -2,3 +2,10 @@
 model1<-'model1'
 
 library(e1071)
+
+nb <- naiveBayes(Satisfaction ~ Airline.Status + Age + Gender +
+           Price.Sensitivity + Year.of.First.Flight + No.of.Flights.p.a. +
+              Type.of.Travel +
+              Shopping.Amount.at.Airport +
+              Class + Scheduled.Departure.Hour +Departure.Delay.in.Minutes +
+             Arrival.Delay.greater.5.Mins, data = clean_data)
