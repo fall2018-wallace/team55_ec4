91ad4acd1aa49316cba3b6af362b21411359d039
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 00:56:28 -0500

    Saving model_generation1.R

--- model_generation1.R
+++ model_generation1.R
@@ -26,3 +26,8 @@ summary(nb)
 nb
 
 predP <- predict(nbP, newdata = clean_data1, type = "class")
+matrixSat <- table(predP,clean_data1$happyCust)
+sumMatrix <- matrixSat[1,1] + matrixSat[2,2]
+
+percSat <- sumMatrix/sum(matrixSat)
+percSat
