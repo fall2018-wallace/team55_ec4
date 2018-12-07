2fe8516ed37ce7dc3b7685b924e07fad2eae77bc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 08:48:15 -0500

    Saving Exploratory_Data_Analysis1.R

--- Exploratory_Data_Analysis1.R
+++ Exploratory_Data_Analysis1.R
@@ -3,29 +3,29 @@ library(ggplot2)
 library(dplyr)
 df<-data
 #using this for Age
-satisfactionAge<-ggplot(df)+geom_bar(mapping=aes(x=Age,fill=Satisfaction),fill="position",width=0.4)+scale_fill_grey() + theme_classic()
+satisfactionAge<-ggplot(df)+geom_bar(mapping=aes(x=Age,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
 # As observed from the above plots, We can interpret that initially with increase in age, people's satisfaction raatings increase and then It starts decreasing after certain age
 # which says spproximately people in age range of 30 to 50 rate high on satisfaction
 # similar trends when comparing south east and other airlines
 
 #using this for Airline Status
-satisfactionAirlineStatus<-ggplot(df)+geom_bar(mapping=aes(x=Airline.Status,fill=Satisfaction),fill="position",width=0.4)+scale_fill_grey() + theme_classic()
+satisfactionAirlineStatus<-ggplot(df)+geom_bar(mapping=aes(x=Airline.Status,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
 # More number of passengers are having Blue airline status and it increases from blue to platinum to gold to silver
 #similar trends when comapring southeast and other airlines
 
 #using this for gender
-satisfactionGender<-ggplot(df)+geom_bar(mapping=aes(x=Gender,fill=Satisfaction),fill="position",width=0.4)+scale_fill_grey() + theme_classic()
+satisfactionGender<-ggplot(df)+geom_bar(mapping=aes(x=Gender,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
 
 #similar trends when comparing south east and other airlines
 
 #using this for price sensitivity
-satisfactionPriceSensitivity<-ggplot(df)+geom_bar(mapping=aes(x=Price.Sensitivity,fill=Satisfaction),fill="position",width=0.4)+scale_fill_grey() + theme_classic()
+satisfactionPriceSensitivity<-ggplot(df)+geom_bar(mapping=aes(x=Price.Sensitivity,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
 
 #decrease in satisfaction with increase and price sensitivity factor of the passenger
 #similar trends when comparing southeast and other airlines
 
 #using this for year of first flight
-satisfactionYearOfFirstFlight<-ggplot(df)+geom_bar(mapping=aes(x=Year.of.First.Flight,fill=Satisfaction),fill="position",width=0.4)+scale_fill_grey() + theme_classic()
+satisfactionYearOfFirstFlight<-ggplot(df)+geom_bar(mapping=aes(x=Year.of.First.Flight,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
 
 #similar trend in southeast and other airlines
 
