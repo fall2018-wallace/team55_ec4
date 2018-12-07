0fe005d0f2298503984fd33b71da1ce3c9fd0011
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 08:36:09 -0500

    Saving Exploratory_Data_Analysis1.R

--- Exploratory_Data_Analysis1.R
+++ Exploratory_Data_Analysis1.R
@@ -1,9 +1,9 @@
 
 library(ggplot2)
 library(dplyr)
-df<-clean_data
+df<-data
 #using this for Age
-satisfactionAge<-ggplot(df,aes(Age,Satisfaction))+geom_count()+facet_grid(southeast ~ .)+stat_summary(aes(y=Satisfaction),fun.y = "mean", colour = "red", size = 2, geom = "point")
+satisfactionAge<-ggplot(data)+geom_bar(mapping=aes(x=Age,fill=Satisfaction),width=0.4)+scale_fill_grey() + theme_classic()+facet_grid(southeast ~ .)
 cor(df$Age,df$Satisfaction)
 # As observed from the above plots, We can interpret that initially with increase in age, people's satisfaction raatings increase and then It starts decreasing after certain age
 # which says spproximately people in age range of 30 to 50 rate high on satisfaction
