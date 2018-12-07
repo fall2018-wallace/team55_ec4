00f7c753f5a5bfec006f3cb1c76217eca29baf24
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 12:10:59 -0500

    Saving Exploratory_Data_Analysis2.R

--- Exploratory_Data_Analysis2.R
+++ Exploratory_Data_Analysis2.R
@@ -1,21 +1,31 @@
 
 library(ggplot2)
 library(dplyr)
+df<-data
+#using this for Age
+p1<-ggplot(df)+geom_bar(mapping=aes(x=Age,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
+# As observed from the above plots, We can interpret that initially with increase in age, people's satisfaction raatings increase and then It starts decreasing after certain age
+# which says spproximately people in age range of 30 to 50 rate high on satisfaction
+# similar trends when comparing south east and other airlines
 
-p1 <- ggplot(clean_data,aes(x=Satisfaction,y=No.of.Flights.p.a.)) + geom_point()
-p1 <- p1+ stat_summary(fun.y=median, colour="darkred", geom="point", shape=18, size=5) + facet_grid(southeast ~ .)
-p1
+#using this for Airline Status
+p2<-ggplot(df)+geom_bar(mapping=aes(x=Airline.Status,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
+# More number of passengers are having Blue airline status and it increases from blue to platinum to gold to silver
+#similar trends when comapring southeast and other airlines
 
-# Satisfaction vs X..of.Flight.with.other.Airlines
-p2 <- ggplot(clean_data,aes(x=Satisfaction,y=X..of.Flight.with.other.Airlines)) + geom_point() + stat_summary(fun.y=mean, colour="darkred", geom="point", shape=18, size=5) + facet_grid(southeast ~ .)
-p2
+#using this for gender
+p3<-ggplot(df)+geom_bar(mapping=aes(x=Gender,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
 
-# Type of travel vs Satisfaction
-fltmean <- clean_data %>% group_by(Type.of.Travel,southeast) %>% summarize(mean1 = mean(Satisfaction))
-fltmean <- as.data.frame(fltmean)
-p3 <- ggplot(fltmean,aes(x=Type.of.Travel,y=mean1)) + geom_point(size=5) + facet_grid(southeast ~ .)
-p3
+#similar trends when comparing south east and other airlines
 
-# Satisfaction vs No..of.other.Loyalty.Cards 
-p4 <- ggplot(clean_data,aes(x=Satisfaction,y=No..of.other.Loyalty.Cards)) + geom_point() + facet_grid(southeast ~ .) + stat_summary(fun.y=mean, colour="darkred", geom="point", shape=18, size=5)
-p4
+#using this for price sensitivity
+p4<-ggplot(df)+geom_bar(mapping=aes(x=Price.Sensitivity,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
+
+#decrease in satisfaction with increase and price sensitivity factor of the passenger
+#similar trends when comparing southeast and other airlines
+
+#using this for year of first flight
+satisfactionYearOfFirstFlight<-ggplot(df)+geom_bar(mapping=aes(x=Year.of.First.Flight,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
+
+#similar trend in southeast and other airlines
+
