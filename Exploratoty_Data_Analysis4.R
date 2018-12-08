6b6d0b8bb4797294c1967b22f72697d3dab22d57
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 23:45:29 -0500

    force updated content when merge failed

--- Exploratoty_Data_Analysis4.R
+++ Exploratoty_Data_Analysis4.R
@@ -2,7 +2,7 @@
 library(ggplot2)
 library(dplyr)
 
-sat<-data
+sat=data
 
 #convert the target variable to numeric
 #to perfrom various EDA operations
@@ -16,11 +16,9 @@ class(sat$Flight.date)
 #our major concentration is on southeast airline 
 sat$southeast<-as.factor(trimws(sat$Airline.Name)=='Southeast Airlines Co.')
 
+
+fD=ggplot(data)+geom_bar(mapping=aes(x=Class,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()+facet_grid(Flight.date ~ .)
 #plot the graph for Satisfaction Vs FlightDate
-fD=ggplot(sat, aes(x=Flight.date, y=Satisfaction))+facet_grid(.~southeast)+
- stat_summary(fun.y="mean",geom="point", color ="red", size =2)
-fD=fD+ geom_line()
-fD=fD+ ggtitle("Flight Date")
 fD
 #as plotting the whole date has no meaning,
 #plot it with day off the week_days
@@ -28,36 +26,24 @@ sat$week_days=weekdays(sat$Flight.date)
 #day of the month is already present as a variable
 #the spread is more for southeast Airlines
 #also few values fall near Satisfaction 3 and below
+fD1=ggplot(data)+geom_bar(mapping=aes(x=Class,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()+facet_grid(weekdays~ .)
 
-fD1=ggplot(sat, aes(x=week_days, y=Satisfaction))+facet_grid(.~southeast)+
-  stat_summary(fun.y="mean",geom="point", color ="red", size =2)+theme(axis.text.x = element_text(angle = 90, hjust = 1))
-fD1=fD1+ geom_count()
-fD1=fD1+ ggtitle("Flight weekdays")
 fD1
 #the Satisfaction for southeast is more on tuesdays
 #the remaining are almost same for all days
 
-fD2=ggplot(sat, aes(x=Airline.Name, y=Satisfaction))
-fD2=fD2+ geom_point()
-fD2=fD2+theme(axis.text.x = element_text(angle = 90, hjust = 1))+
-  stat_summary(fun.y="mean",geom="point", color ="red", size =2)
-fD2=fD2+ ggtitle("Airline Satisfaction")
+fD3=ggplot(data)+geom_bar(mapping=aes(x=Class,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()+facet_grid(Airline.Name ~ .)
+
 fD2
 #the Satisfaction for different airlines is seen
 #almost all of them have an average Satisfaction of 3.5
 
-fD3=ggplot(sat, aes(x=sat$Scheduled.Departure.Hour, y=Satisfaction))+facet_grid(.~southeast)+
-  stat_summary(fun.y="mean",geom="point", color ="red", size =2)
-fD3=fD3+ geom_point()
-fD3=fD3+ ggtitle("Scheduled departure hour")
+fD3=ggplot(data)+geom_bar(mapping=aes(x=Class,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()+facet_grid(Scheduled.Departure ~ .)
 fD3
 #the graph is more spread for southeast Airlines
 #few of them lead to Satisfaction 3
 
-fD4=ggplot(sat, aes(x=Departure.Delay.in.Minutes, y=Satisfaction))+facet_grid(.~southeast)+
-  stat_summary(fun.y="mean",geom="point", color ="red", size =2)
-fD4=fD4+geom_count()
-fD4=fD4+ ggtitle("Departure delay")
+fD4=ggplot(data)+geom_bar(mapping=aes(x=Class,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()+facet_grid(Departure.Delay.in.Minutes ~ .)
 fD4
 #the rest of Satisfaction seems similar for all Airlines
 #all the departure delays are below 500 Minutes for southeast Airlines
