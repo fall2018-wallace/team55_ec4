b27550ec06e6eecf32afbf000da4a978bb00a530
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-08 07:03:39 -0500

    Saving Exploratory_Data_Analysis5.R

--- Exploratory_Data_Analysis5.R
+++ Exploratory_Data_Analysis5.R
@@ -6,9 +6,9 @@ df<-data
 satisfactionArrivalDelay<-ggplot(df)+geom_bar(mapping=aes(x=Arrival.Delay.in.Minutes,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
 #The percentage of satisfied and not satisfied customers is pretty much the same considering the Arrival Delay.
 #Even when it is High, Average or Low.
-satisfactionCancelled<-ggplot(df)+geom_bar(mapping=aes(x=Flight.cancelled,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
-#In this plot, customer satisfactions can be seen by flights which were cancelled as well.
 
+satisfactionCancelled<-ggplot(df)+geom_bar(mapping=aes(x=Flight.cancelled,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
+#It can be seen that there are more non satisfied people where flights are cancelled. 
 satisfactionFlightTime<-ggplot(df)+geom_bar(mapping=aes(x=Flight.time.in.minutes,fill=Satisfaction),position="fill",width=0.4)+scale_fill_grey() + theme_classic()
 #In this plot, the arrival delay of flights which are greater than 5 minutes have comparatively low satisfaction than the flights
 
