ea1f1cbb666cf6ee933f3c0099c07f0881d25660
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 22:40:25 -0500

    deleting NaiveBayesEv.R

--- NaiveBayesEv.R
+++
@@ -1,8 +0,0 @@
-
-
-predP <- predict(model1, newdata = clean_data1, type = "class")
-matrixSat <- table(predP,clean_data1$happyCust)
-sumMatrix <- matrixSat[1,1] + matrixSat[2,2]
-
-percSat <- sumMatrix/sum(matrixSat)
-percSat
