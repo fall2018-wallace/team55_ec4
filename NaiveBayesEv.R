e3559bea2b23ead6a66a57f4a1f2f7fbdbcd8a20
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 22:40:12 -0500

    Saving NaiveBayesEv.R

--- NaiveBayesEv.R
+++ NaiveBayesEv.R
@@ -1,7 +1,6 @@
 
-nb <- model1
 
-predP <- predict(nb, newdata = clean_data1, type = "class")
+predP <- predict(model1, newdata = clean_data1, type = "class")
 matrixSat <- table(predP,clean_data1$happyCust)
 sumMatrix <- matrixSat[1,1] + matrixSat[2,2]
 
