98d2183d04781aad9377ef72c60a591b7993fc61
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-18 11:36:10 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -21,6 +21,7 @@ unique(clean_data$Satisfaction)
 clean_data_full=clean_data
 #re-run the analysis with a random sample of 75% of the data
 clean_data<-sample(clean_data,97417,replace=FALSE)
+str(clean_data)
 sample = sample.split(clean_data$Age, SplitRatio = .67)
 train = subset(clean_data, sample == TRUE)
 test  = subset(clean_data, sample == FALSE)
